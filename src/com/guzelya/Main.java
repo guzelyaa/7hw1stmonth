b0732b9dd9e827b6da3e6a4701af0ab2e2732781
guzelyaa <abitovaguzelyakilichbekovna@gmail.com>
2022-08-01 20:47:48 +0600

    dz6

--- src/com/guzelya/Main.java
+++ src/com/guzelya/Main.java
@@ -3,23 +3,23 @@ package com.guzelya;
 public class Main {
 
     public static void main(String[] args) {
-	Magic magic = new Magic();
-	Medic medic = new Medic();
-	Warrior warrior = new Warrior();
+        Magic magic = new Magic();
+        Medic medic = new Medic();
+        Warrior warrior = new Warrior();
 
-	Hero[] heroes = {magic, medic, warrior};
+        Hero[] heroes = {magic, medic, warrior};
 
-	for (int i = 0; i < heroes.length; i++) {
-            if (heroes[i] instanceof Magic){
+        for (int i = 0; i < heroes.length; i++) {
+            if (heroes[i] instanceof Magic) {
                 magic.applySuperAbility("Spell");
             }
-            if (heroes[i] instanceof Medic){
+            if (heroes[i] instanceof Medic) {
                 medic.applySuperAbility("Healing");
             }
-            if (heroes[i] instanceof Warrior){
+            if (heroes[i] instanceof Warrior) {
                 warrior.applySuperAbility("Critical Damage");
             }
-	}
+        }
 
     }
 }
